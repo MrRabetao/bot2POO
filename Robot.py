21baf1613c6fa3e1512064ca20f55b0bc0de93a7
Lucas Soares <35111282+MrRabetao@users.noreply.github.com>
2022-10-07 19:48:20 -0300

    Update Robot.py

--- Robot.py
+++ Robot.py
@@ -1,12 +1,12 @@
 class Robot:
     
     def __init__(self):
-        self.nome = "Ubutu"
+        self.nome = "Ubuntu"
         self.tipo = "Virtual"
-        self.tarefa = "O Bot Anamnese tem como intuito ajudar pessoas que\ncarregam o transtorno da dislexia ou que possui dúvidas sobre o assunto.\n",
-        "Com um funcionamento simples e direto, ele vai\nentregar ao usuário um apanhado de dicas sobre a dislexia acompanhado\n",
-        "de uma espécie de diário e um relatório e, ao final,\njuntando tudo isso, o usuário terá uma anamnese que pode auxiliá-lo\n",
-        "enquanto ao que ele deve dizer ao médico."
+        self.tarefa = ("O Bot Anamnese tem como intuito ajudar pessoas que\ncarregam o transtorno da dislexia ou que possui dúvidas sobre o assunto.\n"+
+        "Com um funcionamento simples e direto, ele vai\nentregar ao usuário um apanhado de dicas sobre a dislexia acompanhado\n"+
+        "de uma espécie de diário e um relatório e, ao final,\njuntando tudo isso, o usuário terá uma anamnese que pode auxiliá-lo\n"+
+        "enquanto ao que ele deve dizer ao médico.")
     
     def versionamentoLog(self, versao, log):
         self.versao = versao
@@ -16,21 +16,24 @@ class Robot:
         self.sugestao = sugestao
         
     def dicas(self, idDicas):
+        
         if idDicas == 0:
-            return "dica1"
-        
+            return "dica1"       
         elif idDicas == 1:
-            return "dica1"
-        
+            return "dica1"      
         elif idDicas == 2:
-            return "dica2"
-        
+            return "dica2"        
         elif idDicas == 3:
-            return "dica3"
-        
+            return "dica3"      
         elif idDicas == 4:
             return "dica4"
         
         return "invalido"
+    
+    def mostrarDados(self):
+        sobre =f"O nome do nosso grupo é "+self.nome
+        sobre+=f"!\nEste e todos os bots deste trabalho são do tipo "+self.tipo
+        sobre+=f".\nE a tarefa deste bot é:\n"+self.tarefa
+        return (sobre)
     
     
